fe5923915be99ac16da613401921b302cc2ae7b5
PetraAleksandar <aleksandar.debeljak.91@gmail.com>
2017-08-20 16:14:16 +0200

    Finished scss, removed sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,31 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-  sass: {
-    options: {
-      style: 'compressed'
-    },                              // Task
-    dist: {                            // Target
-      files: {                         // Dictionary of files
-        'build/main.css': 'style/style.sass'
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      options: {
+        style: 'compressed'
+      },                              // Task
+      dist: {                            // Target
+        files: {                         // Dictionary of files
+          'build/main.css': 'style/style.scss'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer') ({browsers: 'last 2 versions'})
+        ]
+      },
+      dist: {
+        src: 'build/main.css'
       }
     }
-  }
   });
+
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.registerTask('default', ['sass']);
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.registerTask('default', ['sass', 'postcss']);
 };
